b31aae39e4d8d02ee3a2ee7e1f3a15ba563d993e
Evgeniy <vlasiuk.zp@gmail.com>
2016-04-19 14:18:38 +0300

    Added files via upload

---
+++ svet1.js
@@ -0,0 +1,74 @@
+
+
+  var sec = 0;
+
+function Svet() {
+  switch (true) {
+    
+  case sec<11 :
+   $('.red').css('background', "#FF0000");
+   $('.orange').css('background', "#492400");
+   $('.green').css('background', "#072600");
+           $(".red").html("10"-sec);
+  
+   break;
+  case 10<sec && sec<15:
+   $('.red').css('background', "#FF0000");
+   $('.orange').css('background', "#FF7B00");
+   $('.green').css('background', "#072600");
+    $(".red").html("--");
+  
+   
+   break;
+  case 14<sec && sec<25:
+   $('.red').css('background', "#140505");
+   $('.orange').css('background', "#492400");
+   $('.green').css('background', "#3FDD18");
+    $(".green").html("24"-sec);
+   
+   break;
+  case sec == 25,5:
+   $('.red').css('background', "#140505");
+   $('.orange').css('background', "#492400");
+   $('.green').css('background', "#131C10");
+   $(".green").html("--");
+   break;
+  case sec == 26:
+   $('.red').css('background', "#140505");
+   $('.orange').css('background', "#492400");
+   $('.green').css('background', "#3FDD18");
+   $(".green").html("--");
+   break;
+  case sec == 26,5:
+   $('.red').css('background', "#140505");
+   $('.orange').css('background', "#492400");
+   $('.green').css('background', "#131C10");
+   $(".green").html("--");
+   break;
+  case sec == 27:
+   $('.red').css('background', "#140505");
+   $('.orange').css('background', "#492400");
+   $('.green').css('background', "#3FDD18");
+   $(".green").html("--");
+   sec=0;
+   break;
+  }
+  
+}
+
+
+
+$(document).ready(function() {
+  console.log('START');
+
+     setInterval(function(){
+          sec = sec+0,5;
+          Svet();
+  
+          console.log(sec);
+          }, 500)
+   
+  
+});
+
+
